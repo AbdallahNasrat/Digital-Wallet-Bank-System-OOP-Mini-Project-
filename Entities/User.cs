ab7a0ea473e0afc52b927bf35578ecde0edc5e4c
Abdallah Nasrat <Abdonasrat1@gmail.com>
2026-07-18 18:43:27 +0300

    Add User Properties

---
+++ Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Wallet___Bank_System_OOP_Mini_Project__.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}

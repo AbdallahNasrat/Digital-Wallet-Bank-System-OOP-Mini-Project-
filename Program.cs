d73a7463a6c9ac6aad631353b43fb057d7014703
Abdallah Nasrat <Abdonasrat1@gmail.com>
2026-07-18 18:33:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Digital_Wallet___Bank_System_OOP_Mini_Project__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
